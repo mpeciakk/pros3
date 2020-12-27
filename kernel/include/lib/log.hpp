8071dcc7f9350f1a287183cad2e225050b4cf44f
mpeciakk <mpeciakk@gmail.com>
2020-12-27 01:43:31 +0100

    Basic logging

--- kernel/include/lib/log.hpp
+++ kernel/include/lib/log.hpp
@@ -1,4 +1,7 @@
 #ifndef __LOG_HPP
 #define __LOG_HPP
 
+int log(int level, const char* __restrict format, ...);
+int printk(const char* __restrict format, ...);
+
 #endif
--- kernel/include/lib/string.hpp
+++ kernel/include/lib/string.hpp
@@ -3,7 +3,13 @@
 
 #include <types.hpp>
 
+#define INT32_MAX_DIGITS 12
+#define INT32_MAX_HEX_DIGITS 8
+
 u32 strlen(const char* str);
 void* memset(void* dest, char val, u32 count);
+void memcpy(void* dest, void* src, u32 size);
+void intToString(int n, char* result);
+void intToHex(int n, char* result);
 
 #endif
